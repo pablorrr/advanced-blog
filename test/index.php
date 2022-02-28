d62c0b10aa80ccb92453f5064de9ad9b70cbcbee
Paweł Kali <pkalisz451@gmail.com>
2022-02-28 11:19:43 +0100

    prace porzadkowe w index.php

--- test/index.php
+++ test/index.php
@@ -7,74 +7,80 @@ $router = new Router;
 $Controller = new Controller;
 
 
-/**
- * ustalenie bazowego uri przy zlozonych uri
- *
- *  w tym przypadku zadzilaja tylko  uri np /base/my/route ,base/hello/world
- *
- *
- */
-//$router->setBase('/base');
-/**
- * my route
- */
 
-$router->get('/base/base', array(
-    'func' => 'Controller::baseroute'
-));//error nie zadzila
+//$router->get('/phpinfo', 'phpinfo');
+//$router->setBase('/base');
+
 
 $router->get('/my/route', array(
     'func' => 'Controller::myroute'
 ));
 
-
-$router->get('/text', array(
-	'func' => array($Controller, 'text'),
-	'parameters' => array(1, 2, 3)
-));
-
-$router->get('/phpinfo', 'phpinfo');
-
-
-
-$router->get('/pa\*th', array(
-	'func' => array($Controller, 'path'),
-	'parameters' => array($router->get_URL(), $router->get_URL(true))
-));
-$router->get('/path(opt)', array(
-	'func' => array($Controller, 'path'),
-	'parameters' => array($router->get_URL(), $router->get_URL(true))
-));
-
-$router->get('/^\/\d+$/', function(){
-	echo 'number';
-});
-
 $router->get('/form', array(
-	'func' => array($Controller, 'form_get')
+    'func' => array($Controller, 'form_get')
 ));
 
 $router->post('/form', array(
-	'func' => array($Controller, 'form_post')
+    'func' => array($Controller, 'form_post')
 ));
 
-$router->get('/para/{var1}/{var2}/{var3}/{var4}', function($var1, $var2, $var3, $var4){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$router->get('/para/{var1}/{var2}/{var3}/{var4}', function ($var1, $var2, $var3, $var4) {
     global $router;
     echo 'via get_URL_parameter():';
-    echo '<br>var1:' .$router->get_URL_parameter('var1');
-    echo "<br>var2:" .$router->get_URL_parameter('var2');
-    echo "<br>var3:" .$router->get_URL_parameter('var3');
-    echo "<br>var4:" .$router->get_URL_parameter('var4');
+    echo '<br>var1:' . $router->get_URL_parameter('var1');
+    echo "<br>var2:" . $router->get_URL_parameter('var2');
+    echo "<br>var3:" . $router->get_URL_parameter('var3');
+    echo "<br>var4:" . $router->get_URL_parameter('var4');
 
     echo '<br>via closure parameters:';
-    echo '<br>var1:' .$var1;
-    echo "<br>var2:" .$var2;
-    echo "<br>var3:" .$var3;
-    echo "<br>var4:" .$var4;
+    echo '<br>var1:' . $var1;
+    echo "<br>var2:" . $var2;
+    echo "<br>var3:" . $var3;
+    echo "<br>var4:" . $var4;
 });
 
-$router->catch_exception(function(){
-	echo 'no suitable routing pattern';
+$router->get('/^\/\d+$/', function () {
+    echo 'number';
+});
+
+$router->get('/pa\*th', array(
+    'func' => array($Controller, 'path'),
+    'parameters' => array($router->get_URL(), $router->get_URL(true))
+));
+
+$router->get('/path(opt)', array(
+    'func' => array($Controller, 'path'),
+    'parameters' => array($router->get_URL(), $router->get_URL(true))
+));
+$router->get('/text', array(
+    'func' => array($Controller, 'text'),
+    'parameters' => array(1, 2, 3)
+));
+$router->catch_exception(function () {
+    echo 'no suitable routing pattern';
 });
 
 $router->match();
