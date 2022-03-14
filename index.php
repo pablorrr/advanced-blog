82e8056dc838bfa89edb45bac56bebc860d488bb
Paweł Kali <pkalisz451@gmail.com>
2022-03-14 10:59:25 +0100

    last commit

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @author           Pierre-Henry Soria <phy@hizup.uk>
- * @copyright        (c) 2015-2017, Pierre-Henry Soria. All Rights Reserved.
- * @license          Lesser General Public License <http://www.gnu.org/copyleft/lesser.html>
- * @link             http://hizup.uk
- */
 
 use Libs\Router;
 use Libs\Config;
